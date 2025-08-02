5ae4f8672f2a96c4bc3b2a3c5d208dc94d02516f
IvanMDOli <ivanmdeoli@hotmail.com>
2025-08-02 04:05:21 -0300

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Menu from './Menu.jsx'
+
+function App() {
+
+
+  return (
+
+      <Menu />
+
+  )
+}
+
+export default App
---
+++ src/Menu.jsx
@@ -0,0 +1,21 @@
+import './Menu.css'
+
+function Menu() {
+
+
+  return (
+    <>
+      <div>
+        <h1>Forkog Tales</h1>
+      </div>
+      <div>
+        <form action="send">
+            
+        </form>
+        <button id="submit"></button>
+      </div>
+    </>
+  )
+}
+
+export default Menu

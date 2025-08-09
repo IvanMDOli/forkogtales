93d5e83c508589f1eef8407a6f53c66ba8aec7fe
IvanMDOli <ivanmdeoli@hotmail.com>
2025-08-09 02:07:31 -0300

    Migrando a Tailwind Front/NavBar

---
+++ src/components/Home/Post/Post.jsx
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom'
+import dec1 from '../../../assets/decoratives/decorativo-leon.png'
+
+export const Post = () => {
+  return (
+    <section className='flex flex-col items-center justify-center px-5 pt-50 pb-20 w-full h-full bg-[url(/backgrounds/fondo-taberna.png)] bg-top bg-fixed bg-cover bg-no-repeat'>
+        <div className='*:flex *:flex-col *:items-center *:justify-center *:w-full flex flex-col items-center justify-between w-7/10 h-full p-[5%] aspect-1195/1666 object-cover bg-[url(../src/assets/posts/contenedor-post.png)] bg-top bg-contain bg-no-repeat'>
+          <div>
+            <h1 className='text-red-800 select-none [-webkit-text-stroke:1px_#00000080]'>Bienvenido</h1>
+          </div>
+          <div>
+            <p className='text-center text-zinc-500'>"Solo quien se atreve a lanzar el dado, <br />conoce el destino que le aguarda." <br />Tabernero de Forkog</p>
+          </div>
+          <div>
+            <img className='w-1/2 select-none' src={dec1} alt="Decorativo Leon" />
+          </div>
+          <div>
+            <p className='text-zinc-900 text-center'>Discover the world of Forkog Tales, where every story is a journey waiting to be explored.</p>
+          </div>
+          <div>
+            <Link to='/mundo' className='px-4 py-2 text-yellow-500 bg-red-800 border border-black rounded hover:bg-red-700 font-serif text-lg'>Conocer más</Link>
+          </div>
+        </div>
+      </section>
+  )
+}
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,43 +1,53 @@
 import { Link } from 'react-router-dom'
 import logo from '../../assets/logo/logo.png'
-import './navbar.scss'
 
 
 export const NavBar = () => {
 
     //const perfil = localStorage.getItem("token")
 
+     const navLinks = [
+        { path: "/home", label: "Inicio" },
+        { path: "/noticias", label: "Noticias" },
+        { path: "/historia", label: "Mundo" },
+        { path: "/mapa", label: "Mapa" }
+    ]
+
+    const perfilLinks = [
+        { path: "/login", label: "Login" },
+        { path: "/register", label: "Registrarse" }
+    ]
+
+    const linkClass = 'text-[16px] select-none duration-300 ease-out hover:text-amber-400'
+
     return (
-        <header className='header'>
-            <div className='logo'>
-                <Link to={'/home'}><img src={logo} alt="Logo" /></Link>
+        <header className='fixed w-full z-100 inset-x-0 top-0 left-0 flex justify-between items-end py-[10px] px-[50px] border-b-4 border-b-amber-500 bg-zinc-900'>
+            <div className='content-[""] -z-1 inset-[0px] absolute bg-[url(/backgrounds/fondo-nav.png)] bg-cover bg-center pointer-events-none opacity-50'></div>
+            <div className='py-4'>
+                <Link to={'/home'}><img className='w-[200px]' src={logo} alt="Logo" /></Link>
             </div>
-            <nav className='nav-bar'>
-                <ul className='botones-nav'>
-                    <li>
-                        <Link className='links-nav' to={"/home"}>Inicio</Link>
-                    </li>
-                    <li>
-                        <Link className='links-nav' to={"/noticias"}>Noticias</Link>
-                    </li>
-                    <li>
-                        <Link className='links-nav' to={"/historia"}>Mundo</Link>
-                    </li>
-                    <li>
-                        <Link className='links-nav' to={"/mapa"}>Mapa</Link>
-                    </li>
+            <nav className='w-[65%] flex items-end justify-between'>
+                <ul className='w-[60%] flex items-center justify-between m-[10px]'>
+                    {navLinks.map((link, index) => (
+                        <li key={index}>
+                            <Link className={`${linkClass} pb-[20px]`} to={link.path}>
+                                {link.label}
+                            </Link>
+                        </li>
+                    ))}
                 </ul>
-                <ul className='perfil-menu'>
+                <ul className='relative m-[10px] hover:[&_ul]:opacity-100 hover:[&_ul]:-translate-x-[50%] hover:[&_ul]:translate-y-[5px] hover:[&_ul]:pointer-events-auto'>
                     <li>
                         {/*<Link className='links-nav' to={//perfil ? "/perfil" : "*"}><img src={perfil ? perfil.picture : "./foto-perfil-generico.png"} alt="Foto de Perfil" />{perfil ? perfil.name : "Perfil"}</Link>*/}
-                        <Link className='links-nav' to='*'>Perfil</Link>
-                        <ul className='dropdown-nav'>
-                            <li>
-                                <Link className='links-nav' to={"/login"}>Login</Link>
-                            </li>
-                            <li>
-                                <Link className='links-nav' to={"/register"}>Registrarse</Link>
-                            </li>
+                        <Link className={`${linkClass} pb-[20px]`} to='*'>Perfil</Link>
+                        <ul className='absolute flex p-[1em] mt-[5px] flex-col items-center top-[100%] left-[50%] duration-300 ease-out opacity-0 pointer-events-none -translate-x-[50%] -translate-y-[10px] bg-zinc-800 text-white rounded shadow-lg'>
+                            {perfilLinks.map((link, index) => (
+                                <li key={index}>
+                                    <Link className={linkClass} to={link.path}>
+                                        {link.label}
+                                    </Link>
+                                </li>
+                            ))}
                         </ul>
                     </li>
                 </ul>
